ca80f6061c31881658c665f4d9508198175cf1b0
eldovyn <farras.pramudita@gmail.com>
2025-07-31 02:39:10 +0700

    style(register): update responsive design for desktop

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { useState } from "react";
 import IconApp from "@/../public/icon-software.png";
+import IconApp1 from "@/../public/icon software (1).png";
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import IconGoogle from "@/../public/images__1_-removebg-preview 2.png";
@@ -22,13 +23,14 @@ export default function RegisterComponent() {
     const isSm = useMediaQuery({ minWidth: 640 });
     const isMd = useMediaQuery({ minWidth: 768 });
     const isDefault = useMediaQuery({ maxWidth: 639 });
+    const isLg = useMediaQuery({ minWidth: 1024 });
 
     const isUsernameError = true;
     const isEmailError = true;
     const isPasswordError = true;
     const isConfirmPasswordError = true;
 
-    if (isSm || isMd || isDefault) {
+    if ((isSm || isMd || isDefault) && !isLg) {
         return (
             <div className={`${isMd ? 'min-h-screen' : 'h-screen'} bg-gray-50 flex flex-col items-center justify-center px-4 sm:px-6`}>
                 <Card className="w-full max-w-sm sm:max-w-md bg-gray-50 border-none shadow-none">
@@ -260,4 +262,196 @@ export default function RegisterComponent() {
             </div>
         );
     }
-}
+
+    return (
+        <div className="flex h-screen bg-gray-100">
+            <div className="flex-1 flex items-center justify-center bg-black">
+                <Image
+                    src={IconApp1}
+                    alt="logo-trenalyze"
+                    width={600}
+                    height={600}
+                    className="mb-4"
+                />
+            </div>
+
+            <div className="flex-1 flex flex-col items-center justify-center bg-white p-6">
+
+                <form className="w-full max-w-md space-y-4">
+                    <p className="text-[15px] mb-2 self-start">Welcome to short.me</p>
+                    <h1 className="text-[25px] font-semibold mb-8 self-start">Register to your account</h1>
+                    <div className={`relative rounded-md ${isUsernameError ? 'mb-0' : ''}`}>
+                        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                            <Image
+                                src={IconUsername}
+                                alt="icon-username"
+                                width={25}
+                                height={25}
+                                className="text-gray-400 translate-y-[2px]"
+                            />
+                        </div>
+                        <input
+                            type="text"
+                            name="username"
+                            id="username"
+                            autoComplete="username"
+                            placeholder="username"
+                            className={`block w-full rounded-md border ${isUsernameError ? 'border-[#C10007]' : 'border-[#D9D9D9]'} bg-white ${(isSm || isDefault) && !isMd ? 'h-[37px]' : 'py-3'} pl-14 pr-3 text-gray-900 placeholder-[#374151] focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm`}
+                        />
+                    </div>
+                    {isUsernameError && (
+                        <p className="text-[10px] text-right me-3 text-[#C10007]">Username is required</p>
+                    )}
+                    <div className={`relative rounded-md ${isEmailError ? 'mb-0' : ''}`}>
+                        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                            <Image
+                                src={IconEmail}
+                                alt="icon-username"
+                                width={25}
+                                height={25}
+                                className="text-gray-400"
+                            />
+                        </div>
+                        <input
+                            type="text"
+                            name="email"
+                            id="email"
+                            autoComplete="email"
+                            placeholder="email"
+                            className={`block w-full rounded-md border ${isEmailError ? 'border-[#C10007]' : 'border-[#D9D9D9]'} bg-white ${(isSm || isDefault) && !isMd ? 'h-[37px]' : 'py-3'} pl-14 pr-3 text-gray-900 placeholder-[#374151] focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm`}
+                        />
+                    </div>
+                    {isEmailError && (
+                        <p className="text-[10px] text-right me-3 text-[#C10007]">Email is required</p>
+                    )}
+                    <div className={`relative rounded-md ${isPasswordError ? 'mb-0' : ''}`}>
+                        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                            <Image
+                                src={IconLock}
+                                alt="icon-lock-password"
+                                width={25}
+                                height={25}
+                                className="text-gray-400"
+                            />
+                        </div>
+                        <input
+                            type={showPassword ? 'text' : 'password'}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="password"
+                            className={`block w-full rounded-md border ${isPasswordError ? 'border-[#C10007]' : 'border-[#D9D9D9]'} bg-white ${(isSm || isDefault) && !isMd ? 'h-[37px]' : 'py-3'} pl-14 pr-3 text-gray-900 placeholder-[#374151] focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm`}
+                        />
+                        <button
+                            onClick={togglePasswordVisibility}
+                            className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 focus:outline-none"
+                        >
+                            {showPassword ? (
+                                <Image
+                                    src={IconEye}
+                                    alt="icon-lock-password"
+                                    width={20}
+                                    height={20}
+                                    className="text-gray-400"
+                                />
+                            ) : (
+                                <Image
+                                    src={IconEye}
+                                    alt="icon-lock-password"
+                                    width={20}
+                                    height={20}
+                                    className="text-gray-400"
+                                />
+                            )}
+                        </button>
+                    </div>
+                    {isPasswordError && (
+                        <p className="text-[10px] text-right me-3 text-[#C10007]">Password is required</p>
+                    )}
+                    <div className={`relative rounded-md ${isConfirmPasswordError ? 'mb-0' : ''}`}>
+                        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                            <Image
+                                src={IconLock}
+                                alt="icon-lock-password"
+                                width={25}
+                                height={25}
+                                className="text-gray-400"
+                            />
+                        </div>
+                        <input
+                            type={showPassword ? 'text' : 'password'}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="password"
+                            className={`block w-full rounded-md border ${isConfirmPasswordError ? 'border-[#C10007]' : 'border-[#D9D9D9]'} bg-white ${(isSm || isDefault) && !isMd ? 'h-[37px]' : 'py-3'} pl-14 pr-3 text-gray-900 placeholder-[#374151] focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm`}
+                        />
+                        <button
+                            onClick={togglePasswordVisibility}
+                            className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 focus:outline-none"
+                        >
+                            {showPassword ? (
+                                <Image
+                                    src={IconEye}
+                                    alt="icon-lock-password"
+                                    width={20}
+                                    height={20}
+                                    className="text-gray-400"
+                                />
+                            ) : (
+                                <Image
+                                    src={IconEye}
+                                    alt="icon-lock-password"
+                                    width={20}
+                                    height={20}
+                                    className="text-gray-400"
+                                />
+                            )}
+                        </button>
+                    </div>
+                    {isConfirmPasswordError && (
+                        <p className="text-[10px] text-right me-3 text-[#C10007]">Password is required</p>
+                    )}
+
+                    <button className="w-full bg-blue-600 text-white p-3 rounded-lg">
+                        Register
+                    </button>
+                </form>
+
+                <p className="mt-1 text-[12px] text-right w-full me-[112.5%] ">
+                    Already have an account?{" "}
+                    <a href="/" className="text-blue-600 font-semibold">
+                        login
+                    </a>
+                </p>
+
+                <div className="flex items-center my-4 w-full justify-center">
+                    <hr className="border-[#000] w-[163px] me-10" /> 
+                    <span className="mx-2 text-[#000] text-[18px]">OR</span>
+                    <hr className="border-[#000] w-[163px] ms-10" /> 
+                </div>
+
+                <div className="flex space-x-4 mt-5">
+                    <button className="w-[272px] me-8 border border-gray-300 h-[38.11px] rounded-lg flex items-center justify-center">
+                        <Image
+                            src={IconGoogle}
+                            alt="icon-google"
+                            width={25}
+                            height={25}
+                            className="text-gray-400"
+                        />
+                        Continue with Google
+                    </button>
+                    <button className="w-[272px] ms-8 border border-gray-300 h-[38.11px] rounded-lg flex items-center justify-center">
+                        <Image
+                            src={IconDiscord}
+                            alt="icon-google"
+                            width={25}
+                            height={25}
+                            className="text-gray-400"
+                        />
+                        Continue with Discord
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
