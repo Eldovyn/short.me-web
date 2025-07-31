4c1f44c86afc97bc62fea8dcd87c8fc69aac9c1a
eldovyn <farras.pramudita@gmail.com>
2025-08-01 00:37:00 +0700

    feat: hide sidebar on mobile & tablet view

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,13 +1,20 @@
+'use client';
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/AppSidebar";
+import { useMediaQuery } from "react-responsive";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+    const isSm = useMediaQuery({ minWidth: 640 });
+    const isMd = useMediaQuery({ minWidth: 768 });
+    const isDefault = useMediaQuery({ maxWidth: 639 });
+    const isLg = useMediaQuery({ minWidth: 1024 });
+
     return (
         <SidebarProvider>
             <div className="flex h-screen w-screen overflow-hidden">
-                <AppSidebar />
+                {isLg && (!isSm || isMd || isDefault) && <AppSidebar />}
                 <div className="flex flex-col flex-1 h-full overflow-hidden bg-white">
-                    <SidebarTrigger className="p-2" />
+                    {isLg && (!isSm || isMd || isDefault) && <SidebarTrigger />}
                     <main className="flex-1 overflow-auto">
                         {children}
                     </main>
