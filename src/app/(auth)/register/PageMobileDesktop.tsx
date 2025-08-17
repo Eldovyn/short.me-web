314902c191c8222a3ee6fbefe9e4a0c50b5974fb
eldovyn <farras.pramudita@gmail.com>
2025-08-17 13:46:07 +0700

    feactor(register): update handle for desktop & mobile

---
+++ src/app/(auth)/register/PageMobileDesktop.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+import IconApp1 from "@/../public/icon software (1).png";
+import IconUsername from "@/../public/Frame.png";
+import IconEmail from "@/../public/Icon Email.png";
+import IconLock from "@/../public/IconLock.png";
+import { EyeOffIcon, EyeIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import IconDiscord from "@/../public/discord-icon-blue-discord-logo-for-chatting-and-communication-RA6Qd2f8_t-removebg-preview 2.png";
+import IconGoogle from "@/../public/images__1_-removebg-preview 2.png";
+import IconGithub from "@/../public/25231 1.png";
+
+const PageMobileDesktop: React.FC<RegisterProps> = ({ RegisterFormik, showConfirmPassword, showPassword, togglePasswordVisibility, toggleConfirmPasswordVisibility }) => {
+    return (
+        <>
+            <p>hello world</p>
+        </>
+    )
+}
+
+export default PageMobileDesktop
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -16,6 +16,7 @@ import { toast } from "sonner";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import PageDesktop from "./pageDesktop";
+import PageMobileDesktop from "./PageMobileDesktop";
 
 export default function RegisterComponent() {
     const [showPassword, setShowPassword] = useState(false);
@@ -40,9 +41,6 @@ export default function RegisterComponent() {
         setShowConfirmPassword(prev => !prev);
     };
 
-    const isSm = useMediaQuery({ minWidth: 640 });
-    const isMd = useMediaQuery({ minWidth: 768 });
-    const isDefault = useMediaQuery({ maxWidth: 639 });
     const isLg = useMediaQuery({ minWidth: 1024 });
 
     const isUsernameError = (formErrors.username ?? []).length > 0;
@@ -126,204 +124,9 @@ export default function RegisterComponent() {
     });
 
 
-    if ((isSm || isMd || isDefault) && !isLg) {
-        return (
-            <div className={`${isMd ? 'min-h-screen' : 'h-screen'} bg-gray-50 flex flex-col items-center justify-center px-4 sm:px-6`}>
-                <Card className="w-full max-w-sm sm:max-w-md bg-gray-50 border-none shadow-none">
-                    <CardHeader className="flex flex-col space-y-4 mt-10 relative">
-                        <div className="mx-auto w-50 h-50 relative">
-                            <Image
-                                src={IconApp}
-                                alt="icon-app"
-                                fill
-                                className="object-contain object-top"
-                                style={{
-                                    position: "absolute",
-                                    top: 0,
-                                    left: "50%",
-                                    transform: "translateX(-50%)",
-                                }}
-                            />
-                        </div>
-                    </CardHeader>
-
-                    <CardContent className="space-y-4">
-                        <h1 className="text-lg font-semibold text-gray-900 text-left mb-4">
-                            Create your account
-                        </h1>
-                        <form onSubmit={formik.isSubmitting ? () => { } : formik.handleSubmit}>
-                            <div className={`relative rounded-md ${!isUsernameError ? 'mb-3' : ''}`}>
-                                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <User size={20} />
-                                </div>
-
-                                <input
-                                    type="text"
-                                    name="username"
-                                    id="username"
-                                    autoComplete="username"
-                                    placeholder="username"
-                                    value={formik.values.username}
-                                    onChange={formik.handleChange}
-                                    className={`block w-full rounded-md border ${isUsernameError ? 'border-[#C10007]' : 'border-[#D9D9D9]'} bg-white ${(isSm || isDefault) && !isMd ? 'h-[37px]' : 'py-3'} pl-14 pr-3 text-gray-900 placeholder-[#374151] focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm`}
-                                />
-                            </div>
-
-                            {isUsernameError && (
-                                <p className="text-[10px] text-right me-3 text-[#C10007] mb-1">Username is required</p>
-                            )}
-
-                            <div className={`relative rounded-md ${!isEmailError ? 'mb-3' : ''}`}>
-                                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <Mail size={20} />
-                                </div>
-                                <input
-                                    type="text"
-                                    name="email"
-                                    id="email"
-                                    autoComplete="email"
-                                    placeholder="email"
-                                    value={formik.values.email}
-                                    onChange={formik.handleChange}
-                                    className={`block w-full rounded-md border ${isEmailError ? 'border-[#C10007]' : 'border-[#D9D9D9]'} bg-white ${(isSm || isDefault) && !isMd ? 'h-[37px]' : 'py-3'} pl-14 pr-3 text-gray-900 placeholder-[#374151] focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm`}
-                                />
-                            </div>
-                            {isEmailError && (
-                                <p className="text-[10px] text-right me-3 text-[#C10007] mb-1">Email is required</p>
-                            )}
-
-                            <div className={`relative rounded-md ${!isPasswordError ? 'mb-3' : ''}`}>
-                                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <Lock size={20} />
-                                </div>
-                                <input
-                                    type={showPassword ? 'text' : 'password'}
-                                    placeholder="password"
-                                    name="password"
-                                    value={formik.values.password}
-                                    onChange={formik.handleChange}
-                                    className={`block w-full rounded-md border ${isPasswordError ? 'border-[#C10007]' : 'border-[#D9D9D9]'} bg-white ${(isSm || isDefault) && !isMd ? 'h-[37px]' : 'py-3'} pl-14 pr-3 text-gray-900 placeholder-[#374151] focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm`}
-                                />
-                                <button
-                                    onClick={togglePasswordVisibility}
-                                    className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 focus:outline-none"
-                                    type="button"
-                                >
-                                    {!showPassword ? (
-                                        <Eye size={20} />
-                                    ) : (
-                                        <EyeOff size={20} />
-                                    )}
-                                </button>
-                            </div>
-                            {isPasswordError && (
-                                <p className="text-[10px] text-right me-3 text-[#C10007] mb-1">Password is required</p>
-                            )}
-
-                            <div className={`relative rounded-md ${!isConfirmPasswordError ? 'mb-5' : ''}`}>
-                                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <Lock size={20} />
-                                </div>
-                                <input
-                                    type={showPassword ? 'text' : 'password'}
-                                    value={formik.values.confirm_password}
-                                    onChange={formik.handleChange}
-                                    name="confirm_password"
-                                    placeholder="confirm password"
-                                    className={`block w-full rounded-md border ${isConfirmPasswordError ? 'border-[#C10007]' : 'border-[#D9D9D9]'} bg-white ${(isSm || isDefault) && !isMd ? 'h-[37px]' : 'py-3'} pl-14 pr-3 text-gray-900 placeholder-[#374151] focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 sm:text-sm`}
-                                />
-                                <button
-                                    onClick={toggleConfirmPasswordVisibility}
-                                    className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 focus:outline-none"
-                                    type="button"
-                                >
-                                    {!showPassword ? (
-                                        <Eye size={20} />
-                                    ) : (
-                                        <EyeOff size={20} />
-                                    )}
-                                </button>
-                            </div>
-                            {isConfirmPasswordError && (
-                                <p className="text-[10px] text-right me-3 text-[#C10007] mb-4">Password is required</p>
-                            )}
-
-                            <Button className="w-full bg-blue-600 hover:bg-blue-700 text-white mb-1" type="submit">
-                                Register
-                            </Button>
-                        </form>
-                        <div className="text-left text-[#000000] mt-0 text-[12px] flex flex-row gap-1">
-                            Already have an account?{" "}
-                            <Link href="/login">
-                                <p className="text-blue-600 hover:underline">Login</p>
-                            </Link>
-                        </div>
-                    </CardContent>
-
-                    <CardFooter className="flex flex-col items-center gap-4 mt-2">
-                        <div className="flex items-center w-full gap-2 text-gray-400 text-sm font-semibold">
-                            <hr className="flex-grow me-9 border-[#000000]" />
-                            <span className="text-[#000000]">OR</span>
-                            <hr className="flex-grow ms-9 border-[#000000]" />
-                        </div>
-
-                        {(isSm || isDefault) && !isMd ? (
-                            <div className="flex justify-center gap-6 w-full">
-                                <button className="flex me-10 items-center justify-center w-[49.72px] h-[37px] border border-[#D9D9D9] rounded-[3.47px] shadow-sm hover:bg-gray-100">
-                                    <Image
-                                        src={IconGoogle}
-                                        alt="icon-google"
-                                        width={25}
-                                        height={25}
-                                        className="text-gray-400"
-                                    />
-                                </button>
-                                <button className="flex ms-10 items-center justify-center w-[49.72px] h-[37px] border border-[#D9D9D9] rounded-[3.47px] shadow-sm hover:bg-gray-100">
-                                    <Image
-                                        src={IconDiscord}
-                                        alt="icon-google"
-                                        width={25}
-                                        height={25}
-                                        className="text-gray-400"
-                                    />
-                                </button>
-                            </div>
-                        ) : ''}
-                        {isMd ? (
-                            <div className="flex flex-col space-y-6 w-full">
-                                <button
-                                    className="flex gap-2 items-center justify-center border border-[#525252] rounded-[14.9px] h-[46px] w-full hover:bg-gray-100 transition"
-                                >
-                                    <Image
-                                        src={IconGoogle}
-                                        alt="icon-google"
-                                        width={25}
-                                        height={25}
-                                        className="text-[#000000]"
-                                    />
-                                    Continue with Google
-                                </button>
-                                <button
-                                    className="flex gap-2 items-center justify-center border border-[#525252] rounded-[14.9px] h-[46px] w-full hover:bg-gray-100 transition"
-                                >
-                                    <Image
-                                        src={IconDiscord}
-                                        alt="icon-discord"
-                                        width={25}
-                                        height={25}
-                                        className="text-[#000000]"
-                                    />
-                                    Continue with Discord
-                                </button>
-                            </div>
-                        ) : ''}
-                    </CardFooter>
-                </Card>
-            </div>
-        );
+    if (isLg) {
+        return <PageDesktop RegisterFormik={formik} showPassword={showPassword} showConfirmPassword={showConfirmPassword} togglePasswordVisibility={togglePasswordVisibility} toggleConfirmPasswordVisibility={toggleConfirmPasswordVisibility} /> 
     }
 
-    return (
-        <PageDesktop RegisterFormik={formik} showPassword={showPassword} showConfirmPassword={showConfirmPassword} togglePasswordVisibility={togglePasswordVisibility} toggleConfirmPasswordVisibility={toggleConfirmPasswordVisibility} />
-    );
+    return <PageMobileDesktop RegisterFormik={formik} showPassword={showPassword} showConfirmPassword={showConfirmPassword} togglePasswordVisibility={togglePasswordVisibility} toggleConfirmPasswordVisibility={toggleConfirmPasswordVisibility} />
 };
